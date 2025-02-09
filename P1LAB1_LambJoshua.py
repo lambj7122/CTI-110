101df5ffa8a17810e9f0c716f460f110a8c2215f
lambj7122 <lambj7122@student.faytechcc.edu>
2025-02-09 13:29:36 -0500

    Add files via upload

---
+++ P1LAB1_LambJoshua.py
@@ -0,0 +1,9 @@
+#Joshua Lamb
+#2-9-2025
+#P1LAB1
+#Input/Output in Python using IDLE
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("Hello,", firstName, lastName + "! Welcome to CTI-110")
