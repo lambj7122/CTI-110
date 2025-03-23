8a67fad9ff9c1c593c1136bf634e78458ba4552a
lambj7122 <lambj7122@student.faytechcc.edu>
2025-03-23 09:03:08 -0400

    Add files via upload

--- P2HW1_LambJoshua.py
+++ P2HW1_LambJoshua.py
@@ -1,14 +1,11 @@
 # Joshua Lamb
-# 3-1-2025 resubmitted 3-20-2025
+# 3-1-2025 resubmitted 3-20-2025. Resubmitted 3-23-2025.
 # P2HW1
 # Python program to calculate travel expenses and show a formatted display.
 
-# I'm not sure why my original submission wouldn't run.  My saved file worked
-# for me when I opened it and ran it.  But I may have changed something in the
-# file when I was referencing it while working on something else.  The only
-# thing I changed for resubmitting was updating the date and assignment name,
-# that I had forgotten to do when I first copied the program from P1HW2.
-
+# 3-23-25: changed quotes in strings.
+# It runs in pythontutor.com visual debugger
+# I'm writing and running the code in IDLE 3.13.2
 
 '''
 Display explanation of program.
@@ -31,25 +28,24 @@ Display "Remaining Balance: (balance)"
 
 
 print('This program calculates and displays travel expenses')
-
-budget = float(input('\nEnter Budget: '))
-
-dest = input('\nEnter your travel destination: ')
-
-gas = float(input('\nHow much do you think you will spend on gas? '))
-
-hotel = float(input('\nApproximately, how much will you need for accomodation/hotel? '))
-
-food = float(input('\nLast, how much do you need for food? '))
-
+print()
+budget = float(input('Enter Budget: '))
+print()
+dest = input('Enter your travel destination: ')
+print()
+gas = float(input('How much do you think you will spend on gas? '))
+print()
+hotel = float(input('Approximately, how much will you need for accomodation/hotel? '))
+print()
+food = float(input('Last, how much do you need for food? '))
 print()
 
 print('------------Travel Expenses------------')
-print(f'{'Location:':20}{dest}')
-print(f'{'Initial Budget:':20}${budget:.2f}')
-print(f'{'Fuel:':20}${gas:.2f}')
-print(f'{'Accomodation:':20}${hotel:.2f}')
-print(f'{'Food:':20}${food:.2f}')
+print(f"{'Location:':20}{dest}")
+print(f"{'Initial Budget:':20}${budget:.2f}")
+print(f"{'Fuel:':20}${gas:.2f}")
+print(f"{'Accomodation:':20}${hotel:.2f}")
+print(f"{'Food:':20}${food:.2f}")
 print('---------------------------------------')
 
 print()
@@ -58,4 +54,4 @@ expenses = gas + hotel + food
 
 balance = budget - expenses
 
-print(f'{'Remaining Balance:':20}${balance:.2f}')
+print(f"{'Remaining Balance:':20}${balance:.2f}")
